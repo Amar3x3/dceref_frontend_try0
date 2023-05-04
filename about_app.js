ffe6d4873e1ad5ee45a5b2646d6a9e780ff34b02
Amar3x3 <20113032@pvgcoet.ac.in>
2023-05-04 14:16:49 +0530

    content home page updated

--- about_app.js
+++ about_app.js
@@ -76,3 +76,18 @@ function reveal_left(){
 }
 
 
+function myFunction() {
+    var dots = document.getElementById("dots1");
+    var moreText = document.getElementById("more1");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
